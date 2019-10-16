465554b2808e8e917a0c10d0c0ee8234c26cd2a0
Patrick Brandão <patrickbrandao@gmail.com>
2019-10-16 17:38:45 -0300

    Create vtunctl.sh

---
+++ vtunctl.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#
+# Script de gerenciamento de tuneis VPN usando VTUN (vtund)
+# http://vtun.sourceforge.net/
+#
+# Autor: Patrick Brandao <patrickbrandao@gmail.com>
+#        http://www.patrickbrandao.com
+#
+# Execute: sh vtunctl.sh help
+#
+# Exemplos no final do arquivo
+#
