6cb9048cfd9e5aadbdae3ebb07a382a95f2b4441
Patrick Brandão <patrickbrandao@gmail.com>
2019-11-22 00:40:40 -0300

    Update eve-prepare.sh

--- eve-prepare.sh
+++ eve-prepare.sh
@@ -32,7 +32,7 @@ export PATH="/bin:/sbin:/usr/sbin:/usr/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 	ALL_VYOS_VERSIONS="
 		1.1.7-amd64
 		1.2.3-amd64
-		1.3-20191121-adm64
+		1.3-20191121-amd64
 	"
 
 	# Diretorio de downloads
